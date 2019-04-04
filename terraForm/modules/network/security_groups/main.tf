8aa10ad2f3ec0c5033cdaf83a821cd5ca6bb2b1b
Dmitri Mogiliver <47614815+dmogiliver@users.noreply.github.com>
2019-04-03 23:57:54 -0700

    only SSH

--- terraForm/modules/network/security_groups/main.tf
+++ terraForm/modules/network/security_groups/main.tf
@@ -3,19 +3,6 @@ resource "aws_security_group" "ssh_in_out" {
   name = "dmogiliver-ssh_in_out"
 }
 
-resource "aws_security_group" "flask_in_out" {
-  vpc_id = "${var.vpc_id}"
-  name = "dmogiliver-flask_in_out"
-}
-
-resource "aws_security_group" "eks_cluster_in_out" {
-  vpc_id = "${var.vpc_id}"
-  name = "dmogiliver-eks_cluster"
-  description = "Cluster communication with worker nodes"
-  tags = {
-    Name = "dmogiliver-eks-demo"
-  }
-}
 
 resource "aws_security_group_rule" "ssh_in" {
   type = "ingress"
@@ -35,38 +22,3 @@ resource "aws_security_group_rule" "ssh_out" {
   security_group_id = "${aws_security_group.ssh_in_out.id}"
 }
 
-resource "aws_security_group_rule" "flask_in" {
-  type = "ingress"
-  from_port = 5000
-  to_port = 5000
-  protocol = "-1"
-  security_group_id = "${aws_security_group.flask_in_out.id}"
-  source_security_group_id = "${aws_security_group.flask_in_out.id}"
-}
-
-resource "aws_security_group_rule" "flask_out" {
-  type = "egress"
-  from_port = 5000
-  to_port = 5000
-  protocol = "-1"
-  security_group_id = "${aws_security_group.flask_in_out.id}"
-  source_security_group_id = "${aws_security_group.flask_in_out.id}"
-}
-
-resource "aws_security_group_rule" "eks_cluster_in" {
-  type = "ingress"
-  from_port = 443
-  to_port = 443
-  protocol = "tcp"
-  cidr_blocks = ["${local.ipAddress}"]
-  security_group_id = "${aws_security_group.eks_cluster_in_out.id}"
-}
-
-resource "aws_security_group_rule" "eks_cluster_out" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.eks_cluster_in_out.id}"
-}
